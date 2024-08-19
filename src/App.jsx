d0dffe27e55fa4e4497ec50391dfce3948dd7287
Telmo Fernandes <telmorf@live.com>
2024-08-19 21:23:40 +0100

    fim da criação dos componentes do jogo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
-import { useState } from 'react'
-
+// css
 import './App.css'
 
+// data
+import { wordsList } from './data/words'
+
+// hooks
+import { useCallback, useEffect, useState } from 'react'
+
+// components
+import Game from './components/Game'
+import GameOver from './components/GameOver'
+import StartScreen from './components/StartScreen'
+
+const stages = [
+  { id: 1, name: 'start' },
+  { id: 2, name: 'game' },
+  { id: 3, name: 'end' },
+]
+
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList)
+
+  console.log(words)
 
   return (
+    <div className="App">
+      {gameStage === 'start' && <StartScreen />}
+      {gameStage === 'game' && <Game />}
+      {gameStage === 'end' && <GameOver />}
 
-    <div className="">
-      <h1>Find Secret Word</h1>
     </div>
   )
 }
---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,13 @@
+import './StartScreen.css'
+
+const StartScreen = () => {
+  return (
+    <div className='start'>
+      <h1>Find Secret Word</h1>
+      <p>Clique no botão abaixo para começar o jogo</p>
+      <button>Começar o Jogo</button>
+    </div>
+  )
+}
+
+export default StartScreen
